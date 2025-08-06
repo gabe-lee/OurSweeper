490eb403628470036653073ed7559472c2f53f21
Gabe Lee <gla.ander@gmail.com>
2025-08-05 22:26:46 -0500

    finished re-writing package paratable

--- internal/game_client/param_table.go
+++ internal/game_client/param_table.go
@@ -1,150 +1,123 @@
 package game_client
 
 import (
-	para "github.com/gabe-lee/OurSweeper/parameter_table"
+	"github.com/gabe-lee/OurSweeper/paratable"
+)
+
+type (
+	ParamCalc     = paratable.ParamCalc
+	PIdx_U64      = paratable.PIdx_U64
+	PIdx_I64      = paratable.PIdx_I64
+	PIdx_F64      = paratable.PIdx_F64
+	PIdx_U32      = paratable.PIdx_U32
+	PIdx_I32      = paratable.PIdx_I32
+	PIdx_F32      = paratable.PIdx_F32
+	PIdx_U16      = paratable.PIdx_U16
+	PIdx_I16      = paratable.PIdx_I16
+	PIdx_U8       = paratable.PIdx_U8
+	PIdx_I8       = paratable.PIdx_I8
+	PIdx_Bool     = paratable.PIdx_Bool
+	PIdx_Calc     = paratable.PIdx_Calc
+	ParamTable    = paratable.ParamTable
+	CalcInterface = paratable.CalcInterface
 )
 
 const (
-	FIRST_ROOT_U64_PARAM uint16 = iota
+	FIRST_U64_PARAM PIdx_U64 = PIdx_U64(iota)
 	// ... more uint64 param indexes
-	_ROOT_U64_PARAMS_END
+	_U64_PARAMS_END
 )
 
 const (
-	FIRST_ROOT_I64_PARAM uint16 = iota + _ROOT_U64_PARAMS_END
+	FIRST_I64_PARAM PIdx_I64 = PIdx_I64(iota + _U64_PARAMS_END)
 	// ... more uint64 param indexes
-	_ROOT_I64_PARAMS_END
+	_I64_PARAMS_END
 )
 
 const (
-	FIRST_ROOT_F64_PARAM uint16 = iota + _ROOT_I64_PARAMS_END
+	FIRST_F64_PARAM PIdx_F64 = PIdx_F64(iota + _I64_PARAMS_END)
 	// ... more float64 param indexes
-	_ROOT_F64_PARAMS_END
+	_F64_PARAMS_END
 )
 
 const (
-	FIRST_DERIVED_U64_PARAM uint16 = iota + _ROOT_F64_PARAMS_END
-	// ... more uint64 param indexes
-	_DERIVED_U64_PARAMS_END
-)
-
-const (
-	FIRST_DERIVED_I64_PARAM uint16 = iota + _DERIVED_U64_PARAMS_END
-	// ... more uint64 param indexes
-	_DERIVED_I64_PARAMS_END
-)
-
-const (
-	FIRST_DERIVED_F64_PARAM uint16 = iota + _DERIVED_I64_PARAMS_END
-	// ... more float64 param indexes
-	_DERIVED_F64_PARAMS_END
-)
-
-const (
-	FIRST_ROOT_U32_PARAM uint16 = iota + _DERIVED_F64_PARAMS_END
+	FIRST_U32_PARAM PIdx_U32 = PIdx_U32(iota + _F64_PARAMS_END)
 	// ... more uint32 param indexes
-	_ROOT_U32_PARAMS_END
+	_U32_PARAMS_END
 )
 
 const (
-	FIRST_ROOT_I32_PARAM uint16 = iota + _ROOT_U32_PARAMS_END
+	FIRST_I32_PARAM PIdx_I32 = PIdx_I32(iota + _U32_PARAMS_END)
 	// ... more int32 param indexes
-	_ROOT_I32_PARAMS_END
+	_I32_PARAMS_END
 )
 
 const (
-	FIRST_ROOT_F32_PARAM uint16 = iota + _ROOT_I32_PARAMS_END
+	FIRST_F32_PARAM PIdx_F32 = PIdx_F32(iota + _I32_PARAMS_END)
 	// ... more float32 param indexes
-	_ROOT_F32_PARAMS_END
+	_F32_PARAMS_END
 )
 
 const (
-	FIRST_DERIVED_U32_PARAM uint16 = iota + _ROOT_F32_PARAMS_END
-	// ... more uint32 param indexes
-	_DERIVED_U32_PARAMS_END
-)
-
-const (
-	FIRST_DERIVED_I32_PARAM uint16 = iota + _DERIVED_U32_PARAMS_END
-	// ... more int32 param indexes
-	_DERIVED_I32_PARAMS_END
-)
-
-const (
-	FIRST_DERIVED_F32_PARAM uint16 = iota + _DERIVED_I32_PARAMS_END
-	// ... more float32 param indexes
-	_DERIVED_F32_PARAMS_END
-)
-
-const (
-	FIRST_ROOT_U16_PARAM uint16 = iota + _DERIVED_F32_PARAMS_END
+	FIRST_U16_PARAM PIdx_U16 = PIdx_U16(iota + _F32_PARAMS_END)
 	// ... more uint16 param indexes
-	_ROOT_U16_PARAMS_END
+	_U16_PARAMS_END
 )
 
 const (
-	FIRST_ROOT_I16_PARAM uint16 = iota + _ROOT_U16_PARAMS_END
+	FIRST_I16_PARAM PIdx_I16 = PIdx_I16(iota + _U16_PARAMS_END)
 	// ... more uint16 param indexes
-	_ROOT_I16_PARAMS_END
+	_I16_PARAMS_END
 )
 
 const (
-	FIRST_DERIVED_U16_PARAM uint16 = iota + _ROOT_I16_PARAMS_END
-	// ... more uint16 param indexes
-	_DERIVED_U16_PARAMS_END
-)
-
-const (
-	FIRST_DERIVED_I16_PARAM uint16 = iota + _DERIVED_U16_PARAMS_END
-	// ... more uint16 param indexes
-	_DERIVED_I16_PARAMS_END
-)
-
-const (
-	FIRST_ROOT_U8_PARAM uint16 = iota + _DERIVED_I16_PARAMS_END
+	FIRST_U8_PARAM PIdx_U8 = PIdx_U8(iota + _I16_PARAMS_END)
 	// ... more uint8 param indexes
-	_ROOT_U8_PARAMS_END
+	_U8_PARAMS_END
 )
 
 const (
-	FIRST_ROOT_I8_PARAM uint16 = iota + _ROOT_U8_PARAMS_END
+	FIRST_I8_PARAM PIdx_I8 = PIdx_I8(iota + _U8_PARAMS_END)
 	// ... more uint8 param indexes
-	_ROOT_I8_PARAMS_END
+	_I8_PARAMS_END
 )
 
 const (
-	FIRST_ROOT_BOOL_PARAM uint16 = iota + _ROOT_I8_PARAMS_END
+	FIRST_BOOL_PARAM PIdx_Bool = PIdx_Bool(iota + _I8_PARAMS_END)
 	// ... more bool param indexes
-	_ROOT_BOOL_PARAMS_END
+	_BOOL_PARAMS_END
 )
 
 const (
-	FIRST_DERIVED_U8_PARAM uint16 = iota + _ROOT_BOOL_PARAMS_END
-	// ... more uint8 param indexes
-	_DERIVED_U8_PARAMS_END
+	_CALC_INVALID PIdx_Calc = PIdx_Calc(iota) // recommended to leave idx 0 as a nil func for debug purposes
+	_FIRST_CALC
+	// ... more calculation indexes
+	_CALC_COUNT
 )
 
 const (
-	FIRST_DERIVED_I8_PARAM uint16 = iota + _DERIVED_U8_PARAMS_END
-	// ... more uint8 param indexes
-	_DERIVED_I8_PARAMS_END
+	_INPUT_FIRST_CALC_1 uint16 = iota
+	_INPUT_FIRST_CALC_2
 )
 
-const (
-	FIRST_DERIVED_BOOL_PARAM uint16 = iota + _DERIVED_I8_PARAMS_END
-	// ... more bool param indexes
-	_DERIVED_BOOL_PARAMS_END
-)
+var _FIRST_CALC_1 = [...]uint16{
+	_INPUT_FIRST_CALC_1: uint16(FIRST_U64_PARAM),
+	_INPUT_FIRST_CALC_2: uint16(FIRST_I64_PARAM),
+}
 
-var MyParamTable = para.NewParamTable(_ROOT_U64_PARAMS_END, _ROOT_I64_PARAMS_END, _ROOT_F64_PARAMS_END, _DERIVED_U64_PARAMS_END, _DERIVED_I64_PARAMS_END, _DERIVED_F64_PARAMS_END, _ROOT_U32_PARAMS_END, _ROOT_I32_PARAMS_END, _ROOT_F32_PARAMS_END, _DERIVED_U32_PARAMS_END, _DERIVED_I32_PARAMS_END, _DERIVED_F32_PARAMS_END, _ROOT_U16_PARAMS_END, _ROOT_I16_PARAMS_END, _DERIVED_U16_PARAMS_END, _DERIVED_I16_PARAMS_END, _ROOT_U8_PARAMS_END, _ROOT_I8_PARAMS_END, _ROOT_BOOL_PARAMS_END, _DERIVED_U8_PARAMS_END, _DERIVED_I8_PARAMS_END, _DERIVED_BOOL_PARAMS_END)
+var MyParamTable = paratable.NewParamTable(_U64_PARAMS_END, _I64_PARAMS_END, _F64_PARAMS_END, _U32_PARAMS_END, _I32_PARAMS_END, _F32_PARAMS_END, _U16_PARAMS_END, _I16_PARAMS_END, _U8_PARAMS_END, _I8_PARAMS_END, _BOOL_PARAMS_END, _CALC_COUNT)
 
 func InitMyParamTable() {
-	MyParamTable.Set_U64(FIRST_ROOT_U64_PARAM, 600)    // a radius
-	MyParamTable.Set_F32(FIRST_ROOT_F32_PARAM, 3.1415) // pi
-	MyParamTable.InitDerived_I32(FIRST_DERIVED_I32_PARAM, []uint16{FIRST_ROOT_U64_PARAM, FIRST_ROOT_F32_PARAM}, func(table *para.ParamTable, oldVal int32) int32 {
-		// Area of a circle with radius 600, as an integer
-		radius := table.Get_U64(FIRST_ROOT_U64_PARAM)
-		pi := table.Get_F32(FIRST_ROOT_F32_PARAM)
-		return int32(float32(radius*radius) * pi)
+	// Register all calculations first
+	MyParamTable.RegisterCalc(_FIRST_CALC, func(t *CalcInterface) {
+		val1 := t.GetInput_U64(_INPUT_FIRST_CALC_1) // first input
+		val2 := t.GetInput_I64(_INPUT_FIRST_CALC_2) // second input
+		val3 := int32(val1) + int32(val2)
+		t.SetOutput_I32(val3)
 	})
+	// Init root values
+	MyParamTable.SetRoot_U64(FIRST_U64_PARAM, 1)
+	MyParamTable.SetRoot_I64(FIRST_I64_PARAM, -2)
+	// Init derived values
+	MyParamTable.InitDerived_I32(FIRST_I32_PARAM, _FIRST_CALC, _FIRST_CALC_1[:])
 }
---
+++ paratable/table_test.go
@@ -0,0 +1,437 @@
+package paratable
+
+import "testing"
+
+func TestParamTable(t *testing.T) {
+	EnableDebug = true
+	const (
+		FIRST_U64_PARAM       PIdx_U64 = PIdx_U64(iota)
+		RECT_WIDTH_1                   // example root val
+		RECT_HEIGHT_1                  // example root val
+		RECT_AREA_1                    // example derived val
+		RECT_AREA_DUPLICATE_1          // example derived val
+		RECT_VOLUME_1                  // example derived val
+		RECT_WIDTH_2                   // example root val
+		RECT_HEIGHT_2                  // example root val
+		RECT_AREA_2                    // example derived val
+		RECT_VOLUME_2                  // example derived val
+		RECT_SUM_VOLUME                // example derived val
+		RECT_AREA_CYCLIC
+		// ... more uint64 param indexes
+		_U64_PARAMS_END
+	)
+
+	const (
+		FIRST_I64_PARAM PIdx_I64 = PIdx_I64(iota + _U64_PARAMS_END)
+		// ... more uint64 param indexes
+		_I64_PARAMS_END
+	)
+
+	const (
+		FIRST_F64_PARAM PIdx_F64 = PIdx_F64(iota + _I64_PARAMS_END)
+		// ... more float64 param indexes
+		_F64_PARAMS_END
+	)
+
+	const (
+		FIRST_U32_PARAM PIdx_U32 = PIdx_U32(iota + _F64_PARAMS_END)
+		RECT_DEPTH_1             // example root val
+		RECT_DEPTH_2             // example root val
+		// ... more uint32 param indexes
+		_U32_PARAMS_END
+	)
+
+	const (
+		FIRST_I32_PARAM PIdx_I32 = PIdx_I32(iota + _U32_PARAMS_END)
+		// ... more int32 param indexes
+		_I32_PARAMS_END
+	)
+
+	const (
+		FIRST_F32_PARAM PIdx_F32 = PIdx_F32(iota + _I32_PARAMS_END)
+		// ... more float32 param indexes
+		_F32_PARAMS_END
+	)
+
+	const (
+		FIRST_U16_PARAM PIdx_U16 = PIdx_U16(iota + _F32_PARAMS_END)
+		// ... more uint16 param indexes
+		_U16_PARAMS_END
+	)
+
+	const (
+		FIRST_I16_PARAM PIdx_I16 = PIdx_I16(iota + _U16_PARAMS_END)
+		// ... more uint16 param indexes
+		_I16_PARAMS_END
+	)
+
+	const (
+		FIRST_U8_PARAM PIdx_U8 = PIdx_U8(iota + _I16_PARAMS_END)
+		// ... more uint8 param indexes
+		_U8_PARAMS_END
+	)
+
+	const (
+		FIRST_I8_PARAM PIdx_I8 = PIdx_I8(iota + _U8_PARAMS_END)
+		// ... more uint8 param indexes
+		_I8_PARAMS_END
+	)
+
+	const (
+		FIRST_BOOL_PARAM PIdx_Bool = PIdx_Bool(iota + _I8_PARAMS_END)
+		// ... more bool param indexes
+		_BOOL_PARAMS_END
+	)
+
+	const (
+		_CALC_INVALID                PIdx_Calc = PIdx_Calc(iota) // recommended to leave idx 0 as a nil func for debug purposes
+		_CALC_AREA_OF_RECTANGLE                                  // example calc
+		_CALC_VOLUME_OF_RECTANGLE                                // example calc
+		_CALC_SUM_OF_VOLUME_OF_RECTS                             // example calc
+		// ... more calculation indexes
+		_CALC_COUNT
+	)
+
+	const (
+		_INPUT_AREA_OF_RECTANGLE_WIDTH uint16 = iota
+		_INPUT_AREA_OF_RECTANGLE_HEIGHT
+	)
+
+	var _AREA_OF_RECTANGLE_1 = [...]uint16{
+		_INPUT_AREA_OF_RECTANGLE_WIDTH:  uint16(RECT_WIDTH_1),
+		_INPUT_AREA_OF_RECTANGLE_HEIGHT: uint16(RECT_HEIGHT_1),
+	}
+
+	var _AREA_OF_RECTANGLE_2 = [...]uint16{
+		_INPUT_AREA_OF_RECTANGLE_WIDTH:  uint16(RECT_WIDTH_2),
+		_INPUT_AREA_OF_RECTANGLE_HEIGHT: uint16(RECT_HEIGHT_2),
+	}
+
+	var _AREA_OF_RECTANGLE_CYCLIC = [...]uint16{
+		_INPUT_AREA_OF_RECTANGLE_WIDTH:  uint16(RECT_AREA_CYCLIC),
+		_INPUT_AREA_OF_RECTANGLE_HEIGHT: uint16(RECT_HEIGHT_2),
+	}
+
+	const (
+		_INPUT_VOLUME_OF_RECTANGLE_AREA uint16 = iota
+		_INPUT_VOLUME_OF_RECTANGLE_DEPTH
+	)
+
+	var _VOLUME_OF_RECTANGLE_1 = [...]uint16{
+		_INPUT_VOLUME_OF_RECTANGLE_AREA:  uint16(RECT_AREA_1),
+		_INPUT_VOLUME_OF_RECTANGLE_DEPTH: uint16(RECT_DEPTH_1),
+	}
+
+	var _VOLUME_OF_RECTANGLE_2 = [...]uint16{
+		_INPUT_VOLUME_OF_RECTANGLE_AREA:  uint16(RECT_AREA_2),
+		_INPUT_VOLUME_OF_RECTANGLE_DEPTH: uint16(RECT_DEPTH_2),
+	}
+
+	const (
+		_INPUT_SUM_OF_RECT_VOLUME_1 uint16 = iota
+		_INPUT_SUM_OF_RECT_VOLUME_2
+	)
+
+	var _SUM_OF_RECT_VOLUMES_1_2 = [...]uint16{
+		_INPUT_SUM_OF_RECT_VOLUME_1: uint16(RECT_VOLUME_1),
+		_INPUT_SUM_OF_RECT_VOLUME_2: uint16(RECT_VOLUME_2),
+	}
+
+	var MyParamTable = NewParamTable(_U64_PARAMS_END, _I64_PARAMS_END, _F64_PARAMS_END, _U32_PARAMS_END, _I32_PARAMS_END, _F32_PARAMS_END, _U16_PARAMS_END, _I16_PARAMS_END, _U8_PARAMS_END, _I8_PARAMS_END, _BOOL_PARAMS_END, _CALC_COUNT)
+	var InitMyParamTable func() = func() {
+		// Register all calculations first
+		MyParamTable.RegisterCalc(_CALC_AREA_OF_RECTANGLE, func(t *CalcInterface) {
+			width := t.GetInput_U64(_INPUT_AREA_OF_RECTANGLE_WIDTH)   // first input
+			height := t.GetInput_U64(_INPUT_AREA_OF_RECTANGLE_HEIGHT) // second input
+			area := width * height
+			t.SetOutput_U64(area)
+		})
+		MyParamTable.RegisterCalc(_CALC_VOLUME_OF_RECTANGLE, func(t *CalcInterface) {
+			area := t.GetInput_U64(_INPUT_VOLUME_OF_RECTANGLE_AREA)   // first input
+			depth := t.GetInput_U32(_INPUT_VOLUME_OF_RECTANGLE_DEPTH) // second input
+			volume := area * uint64(depth)
+			t.SetOutput_U64(volume)
+		})
+		MyParamTable.RegisterCalc(_CALC_SUM_OF_VOLUME_OF_RECTS, func(t *CalcInterface) {
+			vol1 := t.GetInput_U64(_INPUT_SUM_OF_RECT_VOLUME_1) // first input
+			vol2 := t.GetInput_U64(_INPUT_SUM_OF_RECT_VOLUME_2) // second input
+			sumVol := vol1 + vol2
+			t.SetOutput_U64(sumVol)
+		})
+		// Init root values
+		MyParamTable.SetRoot_U64(RECT_WIDTH_1, 600)  // width
+		MyParamTable.SetRoot_U64(RECT_HEIGHT_1, 777) // height
+		MyParamTable.SetRoot_U64(RECT_WIDTH_2, 42)   // width
+		MyParamTable.SetRoot_U64(RECT_HEIGHT_2, 99)  // height
+		// Init derived values
+		MyParamTable.InitDerived_U64(RECT_AREA_1, _CALC_AREA_OF_RECTANGLE, _AREA_OF_RECTANGLE_1[:])
+		MyParamTable.InitDerived_U64(RECT_VOLUME_1, _CALC_VOLUME_OF_RECTANGLE, _VOLUME_OF_RECTANGLE_1[:])
+		MyParamTable.InitDerived_U64(RECT_AREA_2, _CALC_AREA_OF_RECTANGLE, _AREA_OF_RECTANGLE_2[:])
+		MyParamTable.InitDerived_U64(RECT_VOLUME_2, _CALC_VOLUME_OF_RECTANGLE, _VOLUME_OF_RECTANGLE_2[:])
+		MyParamTable.InitDerived_U64(RECT_SUM_VOLUME, _CALC_SUM_OF_VOLUME_OF_RECTS, _SUM_OF_RECT_VOLUMES_1_2[:])
+		MyParamTable.InitDerived_U64(RECT_AREA_DUPLICATE_1, _CALC_AREA_OF_RECTANGLE, _AREA_OF_RECTANGLE_1[:])
+	}
+	InitMyParamTable()
+	var expectRoot_U64 = func(idx PIdx_U64, val uint64) {
+		gotVal := MyParamTable.Get_U64(idx)
+		if gotVal != val {
+			t.Errorf("root value error:\n\tEXP: %d\n\tGOT: %d", val, gotVal)
+		}
+	}
+	var expectRoot_U32 = func(idx PIdx_U32, val uint32) {
+		gotVal := MyParamTable.Get_U32(idx)
+		if gotVal != val {
+			t.Errorf("root value error:\n\tEXP: %d\n\tGOT: %d", val, gotVal)
+		}
+	}
+	var expectDerived_U64 = func(idx PIdx_U64, val uint64) {
+		gotVal := MyParamTable.Get_U64(idx)
+		if gotVal != val {
+			t.Errorf("derived value error:\n\tEXP: %d\n\tGOT: %d", val, gotVal)
+		}
+	}
+	expectRoot_U64(RECT_WIDTH_1, 600)
+	expectRoot_U64(RECT_HEIGHT_1, 777)
+	expectDerived_U64(RECT_AREA_1, 466200)
+	expectDerived_U64(RECT_AREA_DUPLICATE_1, 466200)
+	expectRoot_U32(RECT_DEPTH_1, 0)
+	expectDerived_U64(RECT_VOLUME_1, 0)
+	expectRoot_U64(RECT_WIDTH_2, 42)
+	expectRoot_U64(RECT_HEIGHT_2, 99)
+	expectDerived_U64(RECT_AREA_2, 4158)
+	expectRoot_U32(RECT_DEPTH_2, 0)
+	expectDerived_U64(RECT_VOLUME_2, 0)
+	expectDerived_U64(RECT_SUM_VOLUME, 0)
+	MyParamTable.SetRoot_U64(RECT_WIDTH_1, 555)
+	expectRoot_U64(RECT_WIDTH_1, 555)
+	expectRoot_U64(RECT_HEIGHT_1, 777)
+	expectDerived_U64(RECT_AREA_1, 431235)
+	expectDerived_U64(RECT_AREA_DUPLICATE_1, 431235)
+	expectRoot_U32(RECT_DEPTH_1, 0)
+	expectDerived_U64(RECT_VOLUME_1, 0)
+	expectRoot_U64(RECT_WIDTH_2, 42)
+	expectRoot_U64(RECT_HEIGHT_2, 99)
+	expectDerived_U64(RECT_AREA_2, 4158)
+	expectRoot_U32(RECT_DEPTH_2, 0)
+	expectDerived_U64(RECT_VOLUME_2, 0)
+	expectDerived_U64(RECT_SUM_VOLUME, 0)
+	MyParamTable.SetRoot_U32(RECT_DEPTH_1, 411)
+	expectRoot_U64(RECT_WIDTH_1, 555)
+	expectRoot_U64(RECT_HEIGHT_1, 777)
+	expectDerived_U64(RECT_AREA_1, 431235)
+	expectDerived_U64(RECT_AREA_DUPLICATE_1, 431235)
+	expectRoot_U32(RECT_DEPTH_1, 411)
+	expectDerived_U64(RECT_VOLUME_1, 177237585)
+	expectRoot_U64(RECT_WIDTH_2, 42)
+	expectRoot_U64(RECT_HEIGHT_2, 99)
+	expectDerived_U64(RECT_AREA_2, 4158)
+	expectRoot_U32(RECT_DEPTH_2, 0)
+	expectDerived_U64(RECT_VOLUME_2, 0)
+	expectDerived_U64(RECT_SUM_VOLUME, 177237585)
+	MyParamTable.SetRoot_U32(RECT_DEPTH_2, 35)
+	expectRoot_U64(RECT_WIDTH_1, 555)
+	expectRoot_U64(RECT_HEIGHT_1, 777)
+	expectDerived_U64(RECT_AREA_1, 431235)
+	expectDerived_U64(RECT_AREA_DUPLICATE_1, 431235)
+	expectRoot_U32(RECT_DEPTH_1, 411)
+	expectDerived_U64(RECT_VOLUME_1, 177237585)
+	expectRoot_U64(RECT_WIDTH_2, 42)
+	expectRoot_U64(RECT_HEIGHT_2, 99)
+	expectDerived_U64(RECT_AREA_2, 4158)
+	expectRoot_U32(RECT_DEPTH_2, 35)
+	expectDerived_U64(RECT_VOLUME_2, 145530)
+	expectDerived_U64(RECT_SUM_VOLUME, 177383115)
+	MyParamTable.SetRoot_U64(RECT_HEIGHT_1, 51)
+	expectRoot_U64(RECT_WIDTH_1, 555)
+	expectRoot_U64(RECT_HEIGHT_1, 51)
+	expectDerived_U64(RECT_AREA_1, 28305)
+	expectDerived_U64(RECT_AREA_DUPLICATE_1, 28305)
+	expectRoot_U32(RECT_DEPTH_1, 411)
+	expectDerived_U64(RECT_VOLUME_1, 11633355)
+	expectRoot_U64(RECT_WIDTH_2, 42)
+	expectRoot_U64(RECT_HEIGHT_2, 99)
+	expectDerived_U64(RECT_AREA_2, 4158)
+	expectRoot_U32(RECT_DEPTH_2, 35)
+	expectDerived_U64(RECT_VOLUME_2, 145530)
+	expectDerived_U64(RECT_SUM_VOLUME, 11778885)
+	// bad conditions
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("setting root value with wrong type did not cause panic with EnableDebug == true")
+			}
+		}()
+		MyParamTable.SetRoot_Bool(PIdx_Bool(RECT_HEIGHT_1), true)
+	}()
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("setting root value with idx out of range did not cause panic with EnableDebug == true")
+			}
+		}()
+		MyParamTable.SetRoot_Bool(PIdx_Bool(1000), true)
+	}()
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("directly setting derived value did not cause panic with EnableDebug == true")
+			}
+		}()
+		MyParamTable.SetRoot_U64(RECT_AREA_1, 1)
+	}()
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("getting unregistered calc did not cause panic with EnableDebug == true")
+			}
+		}()
+		var _ = MyParamTable.getCalc(_CALC_INVALID)
+	}()
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("re-registering already registered calc did not cause panic with EnableDebug == true")
+			}
+		}()
+		MyParamTable.RegisterCalc(_CALC_AREA_OF_RECTANGLE, func(t *CalcInterface) {
+			t.SetOutput_U64(0)
+		})
+	}()
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("causing derived cyclic loop did not cause panic with EnableDebug == true")
+			}
+		}()
+		MyParamTable.InitDerived_U64(RECT_AREA_CYCLIC, _CALC_AREA_OF_RECTANGLE, _AREA_OF_RECTANGLE_CYCLIC[:])
+	}()
+	func() {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("out-of-order NewParamTable did not cause panic with EnableDebug == true")
+			}
+		}()
+		var _ = NewParamTable(PIdx_U64(_U32_PARAMS_END), _I64_PARAMS_END, _F64_PARAMS_END, PIdx_U32(_U64_PARAMS_END), _I32_PARAMS_END, _F32_PARAMS_END, _U16_PARAMS_END, _I16_PARAMS_END, _U8_PARAMS_END, _I8_PARAMS_END, _BOOL_PARAMS_END, _CALC_COUNT)
+	}()
+}
+
+/*
+	 type (
+		ParamCalc = paratable.ParamCalc
+		PIdx_U64  = paratable.PIdx_U64
+		PIdx_I64  = paratable.PIdx_I64
+		PIdx_F64  = paratable.PIdx_F64
+		PIdx_U32  = paratable.PIdx_U32
+		PIdx_I32  = paratable.PIdx_I32
+		PIdx_F32  = paratable.PIdx_F32
+		PIdx_U16  = paratable.PIdx_U16
+		PIdx_I16  = paratable.PIdx_I16
+		PIdx_U8   = paratable.PIdx_U8
+		PIdx_I8   = paratable.PIdx_I8
+		PIdx_Bool = paratable.PIdx_Bool
+		PIdx_Calc = paratable.PIdx_Calc
+		ParamTable = paratable.ParamTable
+		CalcInterface = paratable.CalcInterface
+	 )
+
+	 const (
+		FIRST_U64_PARAM PIdx_U64 = PIdx_U64(iota)
+		// ... more uint64 param indexes
+		_U64_PARAMS_END
+	 )
+
+	 const (
+		FIRST_I64_PARAM PIdx_I64 = PIdx_I64(iota + _U64_PARAMS_END)
+		// ... more uint64 param indexes
+		_I64_PARAMS_END
+	 )
+
+	 const (
+		FIRST_F64_PARAM PIdx_F64 = PIdx_F64(iota + _I64_PARAMS_END)
+		// ... more float64 param indexes
+		_F64_PARAMS_END
+	 )
+
+	 const (
+		FIRST_U32_PARAM PIdx_U32 = PIdx_U32(iota + _F64_PARAMS_END)
+		// ... more uint32 param indexes
+		_U32_PARAMS_END
+	 )
+
+	 const (
+		FIRST_I32_PARAM PIdx_I32 = PIdx_I32(iota + _U32_PARAMS_END)
+		// ... more int32 param indexes
+		_I32_PARAMS_END
+	 )
+
+	 const (
+		FIRST_F32_PARAM PIdx_F32 = PIdx_F32(iota + _I32_PARAMS_END)
+		// ... more float32 param indexes
+		_F32_PARAMS_END
+	 )
+
+	 const (
+		FIRST_U16_PARAM PIdx_U16 = PIdx_U16(iota + _F32_PARAMS_END)
+		// ... more uint16 param indexes
+		_U16_PARAMS_END
+	 )
+
+	 const (
+		FIRST_I16_PARAM PIdx_I16 = PIdx_I16(iota + _U16_PARAMS_END)
+		// ... more uint16 param indexes
+		_I16_PARAMS_END
+	 )
+
+	 const (
+		FIRST_U8_PARAM PIdx_U8 = PIdx_U8(iota + _I16_PARAMS_END)
+		// ... more uint8 param indexes
+		_U8_PARAMS_END
+	 )
+
+	 const (
+		FIRST_I8_PARAM PIdx_I8 = PIdx_I8(iota + _U8_PARAMS_END)
+		// ... more uint8 param indexes
+		_I8_PARAMS_END
+	 )
+
+	 const (
+		FIRST_BOOL_PARAM PIdx_Bool = PIdx_Bool(iota + _I8_PARAMS_END)
+		// ... more bool param indexes
+		_BOOL_PARAMS_END
+	 )
+
+	 const (
+		_CALC_INVALID PIdx_Calc = PIdx_Calc(iota) // recommended to leave idx 0 as a nil func for debug purposes
+		_FIRST_CALC
+		// ... more calculation indexes
+		_CALC_COUNT
+	 )
+
+	 const (
+		_INPUT_FIRST_CALC_1 uint16 = iota
+		_INPUT_FIRST_CALC_2
+	 )
+
+	 var _FIRST_CALC_1 = [...]uint16{
+		_INPUT_FIRST_CALC_1:  uint16(FIRST_U64_PARAM),
+		_INPUT_FIRST_CALC_2: uint16(FIRST_I64_PARAM),
+	 }
+
+	 var MyParamTable = paratable.NewParamTable(_U64_PARAMS_END, _I64_PARAMS_END, _F64_PARAMS_END, _U32_PARAMS_END, _I32_PARAMS_END, _F32_PARAMS_END, _U16_PARAMS_END, _I16_PARAMS_END, _U8_PARAMS_END, _I8_PARAMS_END, _BOOL_PARAMS_END, _CALC_COUNT)
+
+	 func InitMyParamTable() {
+		// Register all calculations first
+		MyParamTable.RegisterCalc(_FIRST_CALC, func(t *CalcInterface) {
+			val1 := t.GetInput_U64(_INPUT_FIRST_CALC_1) // first input
+			val2 := t.GetInput_I64(_INPUT_FIRST_CALC_2) // second input
+			val3 := int32(val1) + int32(val2)
+			t.SetOutput_I32(val3)
+		})
+		// Init root values
+		MyParamTable.SetRoot_U64(FIRST_U64_PARAM, 1)
+		MyParamTable.SetRoot_I64(FIRST_I64_PARAM, -2)
+		// Init derived values
+		MyParamTable.InitDerived_I32(FIRST_I32_PARAM, _FIRST_CALC, _FIRST_CALC_1[:])
+	 }
+*/
+var PARAM_TABLE_TEMPLATE_DOC_COMMENT struct{}
